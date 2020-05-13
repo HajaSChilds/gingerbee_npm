eff5bd9f5d3f8ffed626b113f54d75819086e2be
HajaSC <hajasc@protonmail.com>
2020-05-12 22:31:17 -0400

    Missing code

---
+++ README.md
@@ -0,0 +1,15 @@
+## Gingerbee JS Footer
+
+> This shoule be used in the following manner
+
+Install with the command:
+```
+npm install --save gingerbee_npm
+
+```
+
+```javascript
+import {footer} from gingerbee_npm;
+
+footer('Some Name');
+```
---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var moment = require("moment");
+
+/**
+ * Returns a string element with a footer and an updating year
+ * @param {string} name
+ * @return {string}
+ */
+exports.footer = function (name) {
+  return (
+    "Copyright " + "&copy;" + moment().format("YYYY") + " " + name + " All rights reserved"
+  );
+};
